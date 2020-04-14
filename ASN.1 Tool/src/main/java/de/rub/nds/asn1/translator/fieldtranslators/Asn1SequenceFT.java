9bd4869a93f5db99178a3b6f4f495ebb4fc94914
Nirusu <Nirusu@users.noreply.github.com>
2020-04-14 16:49:08 +0200

    Encode original byte value of a tag in content field
    Useful for interpretation of an implicit tag, e.g. OCSP uses a different tag for a revoked certificate and provides the time of revocation as a child

--- ASN.1 Tool/src/main/java/de/rub/nds/asn1/translator/fieldtranslators/Asn1SequenceFT.java
+++ ASN.1 Tool/src/main/java/de/rub/nds/asn1/translator/fieldtranslators/Asn1SequenceFT.java
@@ -2,8 +2,13 @@ package de.rub.nds.asn1.translator.fieldtranslators;
 
 import de.rub.nds.asn1.model.Asn1Sequence;
 import de.rub.nds.asn1.parser.IntermediateAsn1Field;
+import java.math.BigInteger;
 
 public final class Asn1SequenceFT extends Asn1FieldFT<Asn1Sequence> {
+
+    private final IntermediateAsn1Field intermediateAsn1Field;
+
+    private final Asn1Sequence asn1Sequence;
 
     public Asn1SequenceFT(final IntermediateAsn1Field intermediateAsn1Field) {
         this(intermediateAsn1Field, new Asn1Sequence());
@@ -11,5 +16,13 @@ public final class Asn1SequenceFT extends Asn1FieldFT<Asn1Sequence> {
 
     protected Asn1SequenceFT(final IntermediateAsn1Field intermediateAsn1Field, final Asn1Sequence asn1Sequence) {
         super(intermediateAsn1Field, asn1Sequence);
+        this.intermediateAsn1Field = intermediateAsn1Field;
+        this.asn1Sequence = asn1Sequence;
+    }
+
+    @Override
+    public Asn1Sequence translate(final String identifier, final String type) {
+        this.asn1Sequence.setContent(BigInteger.valueOf(this.intermediateAsn1Field.getTag()).toByteArray());
+        return super.translate(identifier, type);
     }
 }
