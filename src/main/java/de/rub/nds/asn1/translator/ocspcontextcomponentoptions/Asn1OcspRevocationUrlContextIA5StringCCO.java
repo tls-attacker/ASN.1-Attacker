7e8f56ad280d25fb8b456139112ba210bc91e589
Nirusu <Nirusu@users.noreply.github.com>
2020-04-20 15:49:00 +0200

    Remove unneeded imports

--- src/main/java/de/rub/nds/asn1/translator/ocspcontextcomponentoptions/Asn1OcspRevocationUrlContextIA5StringCCO.java
+++ src/main/java/de/rub/nds/asn1/translator/ocspcontextcomponentoptions/Asn1OcspRevocationUrlContextIA5StringCCO.java
@@ -3,9 +3,7 @@ package de.rub.nds.asn1.translator.ocspcontextcomponentoptions;
 import de.rub.nds.asn1.TagClass;
 import de.rub.nds.asn1.TagConstructed;
 import de.rub.nds.asn1.model.Asn1PrimitiveIa5String;
-import de.rub.nds.asn1.model.Asn1Sequence;
 import de.rub.nds.asn1.translator.ContextComponentOption;
-import de.rub.nds.asn1.translator.fieldtranslators.Asn1OcspContextSequenceFT;
 import de.rub.nds.asn1.translator.fieldtranslators.Asn1PrimitiveIa5StringFT;
 
 public class Asn1OcspRevocationUrlContextIA5StringCCO extends ContextComponentOption<Asn1PrimitiveIa5String> {
