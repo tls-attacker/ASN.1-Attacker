81b4610a716811023feb15bc222a497b50c107af
Nirusu <Nirusu@users.noreply.github.com>
2020-04-20 16:38:13 +0200

    Add parsing of explicit tags for default NativeTypeContext

--- src/main/java/de/rub/nds/asn1/translator/contextcomponents/ParseNativeTypeContextComponent.java
+++ src/main/java/de/rub/nds/asn1/translator/contextcomponents/ParseNativeTypeContextComponent.java
@@ -21,6 +21,7 @@ public class ParseNativeTypeContextComponent extends ContextComponent {
             new Asn1EncapsulatingOctetStringCCO(ParseNativeTypesContext.NAME),
             new Asn1EndOfContentCCO(),
             new Asn1EnumeratedCCO(),
+            new Asn1ExplicitCCO(ParseNativeTypeContextComponent.NAME),
             new Asn1IntegerCCO(),
             new Asn1NullCCO(),
             new Asn1ObjectIdentifierCCO(),
