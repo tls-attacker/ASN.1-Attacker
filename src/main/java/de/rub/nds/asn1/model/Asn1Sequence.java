4f573ac381ed9a47e273ee15ba6dcecb515f9de4
Robert Merget <robert.merget@tii.ae>
2022-11-09 17:05:05 +0400

    updated license header and made asn1sequence and asn1 set abstract

--- src/main/java/de/rub/nds/asn1/model/Asn1Sequence.java
+++ src/main/java/de/rub/nds/asn1/model/Asn1Sequence.java
@@ -1,12 +1,11 @@
 /**
- * ASN.1 Tool - A project for creating arbitrary ASN.1 structures
+ * ASN.1-Attacker - A project for creating arbitrary ASN.1 structures
  *
  * Copyright 2014-2022 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
  *
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
-
 package de.rub.nds.asn1.model;
 
 import de.rub.nds.asn1.constants.TagClass;
@@ -18,7 +17,7 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-public final class Asn1Sequence extends Asn1Container {
+public abstract class Asn1Sequence extends Asn1Container {
 
     public Asn1Sequence() {
         super(TagClass.UNIVERSAL, TagConstructed.CONSTRUCTED, TagNumber.SEQUENCE);
--- src/main/java/de/rub/nds/asn1/parser/Asn1ObjectIdentifierParser.java
+++ src/main/java/de/rub/nds/asn1/parser/Asn1ObjectIdentifierParser.java
@@ -1,12 +1,11 @@
 /**
- * ASN.1 Tool - A project for creating arbitrary ASN.1 structures
+ * ASN.1-Attacker - A project for creating arbitrary ASN.1 structures
  *
  * Copyright 2014-2022 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
  *
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
-
 package de.rub.nds.asn1.parser;
 
 import de.rub.nds.asn1.model.Asn1ObjectIdentifier;
