b86f409b59337acca67c4c1127ac4f4a9916f665
Robert Merget <robert.merget@tii.ae>
2022-11-10 08:48:29 +0400

    removed final modifier from asn1int

--- src/main/java/de/rub/nds/asn1/model/Asn1Integer.java
+++ src/main/java/de/rub/nds/asn1/model/Asn1Integer.java
@@ -20,7 +20,7 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
-public final class Asn1Integer extends Asn1Field {
+public class Asn1Integer extends Asn1Field {
 
     @XmlElement(name = "value")
     private ModifiableBigInteger value;
