d801039c5a5e9cfd4078169fb48d337e5bfb0ca8
Robert Merget <robert.merget@tii.ae>
2023-01-02 15:27:22 +0400

    updated license header

--- src/main/java/de/rub/nds/asn1/model/Asn1Enumerated.java
+++ src/main/java/de/rub/nds/asn1/model/Asn1Enumerated.java
@@ -1,3 +1,11 @@
+/*
+ * ASN.1 Tool - A project for creating arbitrary ASN.1 structures
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
 package de.rub.nds.asn1.model;
 
 import de.rub.nds.asn1.parser.Asn1Parser;
@@ -11,12 +19,13 @@ public class Asn1Enumerated extends Asn1Field {
 
     @Override
     public Preparator getGenericPreparator() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from
+        // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
     @Override
     public Asn1Parser<?> getParser() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from
+        // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
 }
