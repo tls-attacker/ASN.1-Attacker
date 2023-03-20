dab064e95d17e80e9f9df506f18527cf843487b2
Robert Merget <robert.merget@tii.ae>
2023-03-20 11:16:16 +0400

    added time decoder class

---
+++ src/main/java/de/rub/nds/asn1/time/TimeDecoder.java
@@ -0,0 +1,61 @@
+/*
+ * ASN.1 Tool - A project for creating arbitrary ASN.1 structures
+ *
+ * Copyright 2014-2023 Ruhr University Bochum, Paderborn University, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.asn1.time;
+
+import de.rub.nds.asn1.parser.ParserException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+public class TimeDecoder {
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static DateTime decodeGeneralizedTimeUtc(String generalizedTimeString) {
+        String[] formats =
+                new String[] {
+                    "yyyyMMddHH",
+                    "yyyyMMddHHmm",
+                    "yyyyMMddHHmmss",
+                    "yyyyMMddHHmmss.SSS",
+                    "yyyyMMddHH'Z'",
+                    "yyyyMMddHHmm'Z'",
+                    "yyyyMMddHHmmss'Z'",
+                    "yyyyMMddHHmmss.SSS'Z'"
+                };
+        for (String format : formats) {
+            try {
+
+                DateTimeFormatter formatter = DateTimeFormat.forPattern(format);
+                DateTime dateTime = formatter.parseDateTime(generalizedTimeString);
+                return dateTime;
+            } catch (IllegalArgumentException E) {
+                // Ignore
+            }
+        }
+        throw new ParserException("Cannot parse time");
+    }
+
+    public static DateTime decodeUtc(String utcTimeString) {
+        String[] formats = new String[] {"yyMMddHHmm", "yyMMddHHmmss"};
+        for (String format : formats) {
+            try {
+
+                DateTimeFormatter formatter = DateTimeFormat.forPattern(format);
+                DateTime dateTime = formatter.parseDateTime(utcTimeString);
+                return dateTime;
+            } catch (IllegalArgumentException E) {
+                // Ignore
+            }
+        }
+        throw new ParserException("Cannot parse time");
+    }
+}
