b4d763d18166b10d2fc11b37ab9d6c61b7b89f3b
Robert Merget <robert.merget@tii.ae>
2023-03-20 09:34:03 +0400

    Removed unused code

--- src/main/java/de/rub/nds/asn1/model/ModifiableVariableHolder.java
+++ src/main/java/de/rub/nds/asn1/model/ModifiableVariableHolder.java
@@ -16,13 +16,9 @@ import java.lang.reflect.Field;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @XmlType(name = "ModVarHolder")
 public abstract class ModifiableVariableHolder implements Serializable {
-
-    private static final Logger LOGGER = LogManager.getLogger();
 
     /**
      * Lists all the modifiable variables declared in the class
