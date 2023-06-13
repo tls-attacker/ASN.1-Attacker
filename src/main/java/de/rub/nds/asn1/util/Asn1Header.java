1f1d1d8e785c2745069ca30b5062745e65642a07
Robert Merget <robert.merget@tii.ae>
2023-06-13 15:33:50 +0400

    revised asn1header

--- src/main/java/de/rub/nds/asn1/util/Asn1Header.java
+++ src/main/java/de/rub/nds/asn1/util/Asn1Header.java
@@ -8,41 +8,38 @@
  */
 package de.rub.nds.asn1.util;
 
+import de.rub.nds.asn1.constants.TagClass;
+import de.rub.nds.asn1.constants.TagConstructed;
 import java.math.BigInteger;
 
 public final class Asn1Header {
 
-    private final int tag;
+    private final TagClass tagClass;
+    private final int tagNumber;
     private final BigInteger length;
-    private final boolean isConstructed;
+    private final TagConstructed tagConstructed;
 
-    public Asn1Header(int tag, BigInteger length, boolean isConstructed) {
-        this.tag = tag;
+    public Asn1Header(
+            TagClass tagClass, int tagNumber, BigInteger length, TagConstructed tagConstructed) {
+        this.tagClass = tagClass;
+        this.tagNumber = tagNumber;
         this.length = length;
-        this.isConstructed = isConstructed;
+        this.tagConstructed = tagConstructed;
     }
 
-    public final int getTag() {
-        return tag;
+    public final int getTagNumber() {
+        return tagNumber;
     }
 
     public final BigInteger getLength() {
         return length;
     }
 
-    public final boolean isConstructed() {
-        return isConstructed;
+    public TagClass getTagClass() {
+        return tagClass;
     }
 
-    @Override
-    public String toString() {
-        return "Asn1Header{"
-                + "tag="
-                + tag
-                + ", length="
-                + length
-                + ", isConstructed="
-                + isConstructed
-                + '}';
+    public TagConstructed getTagConstructed() {
+        return tagConstructed;
     }
 }
