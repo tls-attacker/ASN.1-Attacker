e029632baeb4493fbbbd44537077e86a8a00fee0
Robert Merget <robert.merget@tii.ae>
2023-07-11 16:17:47 +0400

    fixed xml

--- src/main/java/de/rub/nds/asn1/model/Asn1Explicit.java
+++ src/main/java/de/rub/nds/asn1/model/Asn1Explicit.java
@@ -10,9 +10,11 @@ package de.rub.nds.asn1.model;
 
 import de.rub.nds.asn1.constants.TagClass;
 import de.rub.nds.asn1.constants.TagConstructed;
+import jakarta.xml.bind.annotation.XmlAnyElement;
 
 public abstract class Asn1Explicit<InnerField extends Asn1Encodable> extends Asn1Field {
 
+    @XmlAnyElement(lax = true)
     protected final InnerField innerField;
 
     public Asn1Explicit(String identifier, Integer expectedTagNumber, InnerField innerField) {
